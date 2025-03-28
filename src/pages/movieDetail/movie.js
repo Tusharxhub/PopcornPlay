633c028dfbea34d43bc4db60bbd673ee7750ce6e
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-28 23:45:04 +0530

    fix: update logo and IMDb link to PopcornPlay in header and movie detail

--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -61,7 +61,7 @@ const Movie = () => {
                     currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" rel="noopener noreferrer" style={{textDecoration: "none"}}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
                 }
                 {
-                    currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" rel="noopener noreferrer" style={{textDecoration: "none"}}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
+                    currentMovieDetail && currentMovieDetail.PopcornPlay_id && <a href={"https://www.PopcornPlay.com/title/" + currentMovieDetail.PopcornPlay_id} target="_blank" rel="noopener noreferrer" style={{textDecoration: "none"}}><p><span className="movie__PopcornPlayButton movie__Button">PopcornPlay<i className="newTab fas fa-external-link-alt"></i></span></p></a>
                 }
             </div>
             <div className="movie__heading">Production companies</div>
