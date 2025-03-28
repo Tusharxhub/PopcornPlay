83a0c2a4e5c0c7f447efc8343270ba85085a72f0
Tusharxhub <t.k.d.dey2033929837@gmail.com>
2025-03-28 23:47:48 +0530

    refactor: streamline header and movie list components, improve loading state handling in cards

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,53 +1,51 @@
-import React, {useEffect, useState} from "react"
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
-import "./card.css"
-import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "./card.css";
+import { Link } from "react-router-dom";
 
-const Cards = ({movie, type}) => {
-
-    const [isLoading, setIsLoading] = useState(true)
-    const [movieList, setMovieList] = useState([])
+const Cards = ({ movie }) => {
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         setTimeout(() => {
-            setIsLoading(false)
-        }, 1500)
-    }, []) 
+            setIsLoading(false);
+        }, 1500);
+    }, []);
 
-    useEffect(() => {
-        const getData = () => {
-            fetch("...")
-                .then(res => res.json())
-                .then(data => setMovieList(data.results));
-        };
-        getData();
-    }, [type]);
-
-    return <>
-    {
-        isLoading
-        ?
-        <div className="cards">
-            <SkeletonTheme color="#202020" highlightColor="#444">
-                <Skeleton height={300} duration={2} />
-            </SkeletonTheme>
-        </div>
-        :
-        <Link to={`/movie/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
-            <div className="cards">
-                <img className="cards__img" src={`https://image.tmdb.org/t/p/original${movie ? movie.poster_path : ""}`} alt="Movie Poster" />
-                <div className="cards__overlay">
-                    <div className="card__title">{movie?movie.original_title:""}</div>
-                    <div className="card__runtime">
-                        {movie?movie.release_date:""}
-                        <span className="card__rating">{movie?movie.vote_average:""}<i className="fas fa-star" /></span>
-                    </div>
-                    <div className="card__description">{movie ? movie.overview.slice(0,118)+"..." : ""}</div>
+    return (
+        <>
+            {isLoading ? (
+                <div className="cards">
+                    <SkeletonTheme color="#202020" highlightColor="#444">
+                        <Skeleton height={300} duration={2} />
+                    </SkeletonTheme>
                 </div>
-            </div>
-        </Link>
-    }
-    </>
-}
+            ) : (
+                <Link to={`/movie/${movie.id}`} style={{ textDecoration: "none", color: "white" }}>
+                    <div className="cards">
+                        <img
+                            className="cards__img"
+                            src={`https://image.tmdb.org/t/p/original${movie ? movie.poster_path : ""}`}
+                            alt={movie ? movie.original_title : "Movie Poster"}
+                        />
+                        <div className="cards__overlay">
+                            <div className="card__title">{movie ? movie.original_title : ""}</div>
+                            <div className="card__runtime">
+                                {movie ? movie.release_date : ""}
+                                <span className="card__rating">
+                                    {movie ? movie.vote_average : ""}
+                                    <i className="fas fa-star" />
+                                </span>
+                            </div>
+                            <div className="card__description">
+                                {movie ? movie.overview.slice(0, 118) + "..." : ""}
+                            </div>
+                        </div>
+                    </div>
+                </Link>
+            )}
+        </>
+    );
+};
 
-export default Cards
+export default Cards;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    const [movieList, setMovieList] = useState([]);
-    const type = "popular"; // Example type, replace with actual logic
-
-    useEffect(() => {
-        const getData = () => {
-            fetch("...")
-                .then(res => res.json())
-                .then(data => setMovieList(data.results));
-        };
-        getData();
-    }, [type]);
-
     return (
         <div className="header">
             <div className="headerLeft">
-                <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/PopcornPlay_Logo_2016.svg/2560px-PopcornPlay_Logo_2016.svg.png" /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
-                <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+                <Link to="/">
+                    <img
+                        className="header__icon"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/PopcornPlay_Logo_2016.svg/2560px-PopcornPlay_Logo_2016.svg.png"
+                        alt="PopcornPlay Logo"
+                    />
+                </Link>
+                <Link to="/movies/popular" style={{ textDecoration: "none" }}>
+                    <span>Popular</span>
+                </Link>
+                <Link to="/movies/top_rated" style={{ textDecoration: "none" }}>
+                    <span>Top Rated</span>
+                </Link>
+                <Link to="/movies/upcoming" style={{ textDecoration: "none" }}>
+                    <span>Upcoming</span>
+                </Link>
             </div>
         </div>
     );
